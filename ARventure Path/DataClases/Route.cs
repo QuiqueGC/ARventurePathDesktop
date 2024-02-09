c528a92b506ed826fcf441b5d3ae0a7726ef252b
Quique <eguevarac2223@politecnics.barcelona>
2024-02-09 18:33:37 +0100

    working in dataClasses

---
+++ ARventure Path/DataClases/Route.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARventure_Path.DataClases
+{
+    internal class Route
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Stop> Stops { get; set; }
+        public DateTime Time { get; set; }
+        public int Steps { get; set; }
+        public float Distance { get; set; }
+    }
+}
---
+++ ARventure Path/DataClases/Story.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARventure_Path.DataClases
+{
+    internal class Story
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Img { get; set; }
+        public string Summary { get; set; }
+        public ArrayList TxtFragments { get; set; }
+    }
+}
