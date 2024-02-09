e1b4de870c001ef93f0ca37fdc94be005889d3f7
Quique <eguevarac2223@politecnics.barcelona>
2024-02-09 19:23:15 +0100

    DataClasses added

---
+++ ARventure Path/DataClases/Happenings/SoundHappening.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARventure_Path.DataClases.Happenings
+{
+    internal class SoundHappening : AbstractHappening
+    {
+        public string Url { get; set; }
+    }
+}
