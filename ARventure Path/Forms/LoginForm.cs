996144fcbf36f39fe18ec69d87e30ab20f636f6d
Quique <eguevarac2223@politecnics.barcelona>
2024-02-14 19:34:51 +0100

    añadida funcionalidad al login

--- ARventure Path/Forms/LoginForm.cs
+++ ARventure Path/Forms/LoginForm.cs
@@ -12,11 +12,51 @@ namespace ARventure_Path.Forms
 {
     public partial class LoginForm : Form
     {
-        public LoginForm()
+        private String userName = "ll";
+        private String passwd = "ll";
+        private MainForm mainForm;
+
+        public LoginForm(MainForm mainForm)
         {
+            this.mainForm = mainForm;
             InitializeComponent();
         }
 
-      
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            if(textBoxUser.Text.Equals("") || textBoxPwd.Text.Equals(""))
+            {
+                MessageBox.Show("Debes rellenar los dos campos.", "Error");
+
+            }else if (!textBoxUser.Text.Equals(userName))
+            {
+                MessageBox.Show("El nombre de usuario no es correcto.", "Error");
+
+            }else if(!textBoxPwd.Text.Equals(passwd)) 
+            {
+                MessageBox.Show("La contraseña no es correcta.", "Error");
+            }
+            else 
+            { 
+                groupBoxLogin.Visible = false;
+                mainForm.menuStripMain.Visible = true;
+                pictureBoxArventurePath.Size = new System.Drawing.Size(928,590);
+                pictureBoxArventurePath.Location = new System.Drawing.Point(20, 0);
+            }
+
+                
+        }
+
+        private void textBoxUser_KeyDown(object sender, KeyEventArgs e){ Login(e);}
+
+        private void textBoxPwd_KeyDown(object sender, KeyEventArgs e){Login(e);}
+
+        private void Login(KeyEventArgs e)
+        {
+            if (e.KeyValue == (char)Keys.Enter)
+            {
+                buttonLogin.PerformClick();
+            }
+        }
     }
 }
