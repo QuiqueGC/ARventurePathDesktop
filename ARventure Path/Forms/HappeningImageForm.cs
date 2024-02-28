8fa82a752812ce7c400d4a5090e8e8ee93f31da6
Leo Ferraz <lferrazm2122@politecnics.barcelona>
2024-02-28 20:24:34 +0100

    texto, imagen funcionalidad
    siguiente: funcionalidad sonido

--- ARventure Path/Forms/HappeningImageForm.cs
+++ ARventure Path/Forms/HappeningImageForm.cs
@@ -6,6 +6,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -16,7 +17,7 @@ namespace ARventure_Path.Forms
 {
     public partial class HappeningImageForm : Form
     {
-        private string imgPath = Path.Combine(Application.StartupPath,"..","..", "filesToServer", "imgHappening");
+        private string imgPath = Path.Combine(Application.StartupPath, "..", "..", "filesToServer", "imgHappening");
         private static Image image;
         string fileName;
         private MyUtils.FormType formType;
@@ -31,16 +32,53 @@ namespace ARventure_Path.Forms
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "JPEG(*.JPG)|*.JPG|PNG(*.PNG)|*.PNG";
-
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            if (formType == MyUtils.FormType.Create)
             {
-                image = Image.FromFile(openFileDialog.FileName);
-                
-                imgHappening.Image = image;
-                string filePath = openFileDialog.FileName;
-                textBoxUrl.Text = filePath;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    image = Image.FromFile(openFileDialog.FileName);
 
-                fileName = Path.GetFileName(filePath);
+                    imgHappening.Image = image;
+                    string filePath = openFileDialog.FileName;
+                    textBoxUrl.Text = filePath;
+
+                    fileName = Path.GetFileName(filePath);
+                }
+            }else if(formType == MyUtils.FormType.Modify)
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    image.Dispose();
+                    deleteImage();
+                    image = Image.FromFile(openFileDialog.FileName);
+
+                    imgHappening.Image = image;
+                    string filePath = openFileDialog.FileName;
+                    textBoxUrl.Text = filePath;
+
+                    fileName = Path.GetFileName(filePath);
+
+                }
+            }
+            
+        }
+
+        private void comboBoxHappenings_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            happening happening = (happening)comboBoxHappenings.SelectedItem;
+            if (happening != null)
+            {
+                comboBoxStories.Enabled = true;
+                groupboxIA.Enabled = true;
+                groupboxImgHappening.Enabled = true;
+                comboBoxStories.SelectedItem = happening.story;
+                textBoxName.Text = happening.name;
+                fileName = happening.url;
+
+                textBoxUrl.Text = (Path.Combine(imgPath, fileName));
+                image = Image.FromFile(Path.Combine(imgPath, fileName));
+
+                imgHappening.Image = image;
             }
         }
 
@@ -59,53 +97,125 @@ namespace ARventure_Path.Forms
         {
             if (formType == MyUtils.FormType.Create)
             {
-                // Crear Evento de imagen
+                if (textBoxName.Text.Trim() != "" &&
+                    fileName != null &&
+                    comboBoxStories.SelectedItem != null)
+                {
+                    happening happening = new happening();
+                    happening.name = textBoxName.Text.Trim();
+                    happening.url = fileName;
+                    happening.type = "image";
+                    happening.idStory = (int)comboBoxStories.SelectedValue;
+                    SaveImage();
+
+                    // Crear Evento de imagen
+                    String msg = HappeningOrm.Insert(happening);
+                    if (msg != "")
+                    {
+                        MessageBox.Show(msg, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Evento creado satisfactoriamente.", "Éxito!");
+                        Close();
+                    }
+                }
+                else if (textBoxName.Text.Trim() == "")
+                {
+                    MessageBox.Show("No has introducido nombre de evento.", "Error");
+
+                }
+                else if (comboBoxStories.SelectedItem == null)
+                {
+                    MessageBox.Show("No has escogido la historia a la que pertenece.", "Error");
+
+                }
             }
             else if (formType == MyUtils.FormType.Modify)
             {
-                // Modificar Evento de imagen
+                happening happening = (happening)comboBoxHappenings.SelectedItem;
+
+                if (happening != null
+                    && textBoxName.Text.Trim() != "" &&
+                    fileName != null &&
+                    comboBoxStories.SelectedItem != null)
+                {
+                    happening.name = textBoxName.Text.Trim();
+                    happening.url = fileName;
+                    happening.type = "image";
+                    happening.idStory = (int)comboBoxStories.SelectedValue;
+                    if (happening.url != fileName)
+                    {
+                        SaveImage();
+                    }
+                    
+
+                    // Modificar Evento de imagen
+                    string msg = Orm.Update();
+                    if (msg != "")
+                    {
+                        MessageBox.Show(msg, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Evento modificado satisfactoriamente.", "Éxito!");
+                        Close();
+                    }
+                }
+                else if (textBoxName.Text.Trim() == "")
+                {
+                    MessageBox.Show("No has introducido nombre de evento.", "Error");
+
+                }
+                else if (comboBoxStories.SelectedItem == null)
+                {
+                    MessageBox.Show("No has escogido la historia a la que pertenece.", "Error");
+
+                }
             }
             else
             {
                 // Borrar Evento de imagen
-            }
-
-            String msg = "";
-            if (textBoxName.Text.Trim() != "" &&
-                fileName != null &&
-                comboBoxStories.SelectedItem != null) 
-            {
-                happening happening = new happening();
-                happening.name = textBoxName.Text.Trim();
-                happening.url = fileName;
-                happening.type = "image";
-                happening.idStory = (int)comboBoxStories.SelectedValue;
-                SaveImage();
-
-                msg = HappeningOrm.Insert(happening);
-                if (msg != "")
+                happening happening = (happening)comboBoxHappenings.SelectedItem;
+                if (happening != null)
                 {
-                    MessageBox.Show(msg, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-                else
-                {
-                    MessageBox.Show("Evento creado satisfactoriamente.", "Éxito!");
-                    Close();
-                }
-            }
-            else if(textBoxName.Text.Trim() == "")
-            {
-                MessageBox.Show("No has introducido nombre de evento.", "Error");
+                    string msg = HappeningOrm.Delete(happening);
 
-            }
-            else if (comboBoxStories.SelectedItem == null)
-            {
-                MessageBox.Show("No has escogido la historia a la que pertenece.", "Error");
-
+                    if (msg != "")
+                    {
+                        MessageBox.Show(msg, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MessageBox.Show("El evento se ha eliminado correctamente.", "Éxito!");
+                        image.Dispose();
+                        deleteImage();
+                        Close();
+                    }
+                }
             }
         }
 
-    private void buttonCancel_Click(object sender, EventArgs e)
+        private void deleteImage()
+        {
+            String destinationPath = Path.Combine(imgPath, fileName);
+            if (File.Exists(destinationPath))
+            {
+                try
+                {
+                    File.Delete(destinationPath);
+                } catch (Exception ex)
+                {
+                    MessageBox.Show("Ocurrió un error al eliminar el archivo: " + ex.Message, "Error");
+                }
+            }
+            else
+            {
+                MessageBox.Show("El archivo no existe en la ruta especificada.", "Error");
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
         {
             Close();
         }
@@ -137,6 +247,10 @@ namespace ARventure_Path.Forms
         {
             buttonCreate.Text = "Borrar";
             bindingSourceHappenings.DataSource = HappeningOrm.Select("image");
+            comboBoxHappenings.SelectedItem = null;
+            comboBoxStories.Enabled = false;
+            groupboxIA.Enabled = false;
+            groupboxImgHappening.Enabled = false;
             Text = "Borrar evento de imagen";
         }
 
@@ -144,6 +258,10 @@ namespace ARventure_Path.Forms
         {
             buttonCreate.Text = "Guardar";
             bindingSourceHappenings.DataSource = HappeningOrm.Select("image");
+            comboBoxHappenings.SelectedItem = null;
+            comboBoxStories.Enabled = false;
+            groupboxIA.Enabled = false;
+            groupboxImgHappening.Enabled = false;
             Text = "Modificar evento de imagen";
         }
 
@@ -152,5 +270,6 @@ namespace ARventure_Path.Forms
             labelSelectHappening.Visible = false;
             comboBoxHappenings.Visible = false;
         }
+
     }
 }
--- ARventure Path/Forms/HappeningTextForm.cs
+++ ARventure Path/Forms/HappeningTextForm.cs
@@ -26,55 +26,104 @@ namespace ARventure_Path.Forms
         {
             if (formType == MyUtils.FormType.Create)
             {
-                // Crear Evento de texto
+                if (textBoxName.Text.Trim() != "" &&
+                                textBoxContent.Text.Trim() != "" &&
+                                comboBoxStories.SelectedItem != null)
+                {
+                    happening happening = new happening();
+                    happening.name = textBoxName.Text.Trim();
+                    happening.text = textBoxContent.Text.Trim();
+                    happening.type = "text";
+                    happening.idStory = (int)comboBoxStories.SelectedValue;
+
+                    string msg = HappeningOrm.Insert(happening);
+                    if (msg != "")
+                    {
+                        MessageBox.Show(msg, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MessageBox.Show("El evento se ha guardado correctamente.", "Éxito!");
+                        Close();
+                    }
+                }
+                else if (textBoxName.Text == "")
+                {
+                    MessageBox.Show("No has introducido nombre de evento.", "Error");
+
+                }
+                else if (comboBoxStories.SelectedItem == null)
+                {
+                    MessageBox.Show("No has escogido la historia a la que pertenece.", "Error");
+
+                }
+                else if (textBoxContent.Text == "")
+                {
+                    MessageBox.Show("No has escrito nada en el contenido.", "Error");
+                }
             }
+
             else if (formType == MyUtils.FormType.Modify)
             {
                 // Modificar Evento de texto
+                happening happening = (happening)comboBoxHappenings.SelectedItem;
+
+                if (happening != null
+                    && textBoxName.Text.Trim() != "" &&
+                                textBoxContent.Text.Trim() != "" &&
+                                comboBoxStories.SelectedItem != null)
+                {
+                    happening.name = textBoxName.Text.Trim();
+                    happening.text = textBoxContent.Text.Trim();
+                    happening.type = "text";
+                    happening.idStory = (int)comboBoxStories.SelectedValue;
+
+                    string msg = Orm.Update();
+                    if (msg != "")
+                    {
+                        MessageBox.Show(msg, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MessageBox.Show("El evento se ha modificado correctamente.", "Éxito!");
+                        Close();
+                    }
+                }
+                else if (textBoxName.Text == "")
+                {
+                    MessageBox.Show("No has introducido nombre de evento.", "Error");
+
+                }
+                else if (comboBoxStories.SelectedItem == null)
+                {
+                    MessageBox.Show("No has escogido la historia a la que pertenece.", "Error");
+
+                }
+                else if (textBoxContent.Text == "")
+                {
+                    MessageBox.Show("No has escrito nada en el contenido.", "Error");
+                }
+
             }
-            else
+            else if (formType == MyUtils.FormType.Delete)
             {
                 // Borrar Evento de texto
-            }
-
-
-            string msg = "";
-            if (textBoxName.Text.Trim() != "" &&
-                textBoxContent.Text.Trim() != "" &&
-                comboBoxStories.SelectedItem != null)
-            {
-                happening happening = new happening();
-                happening.name = textBoxName.Text.Trim();
-                happening.text = textBoxContent.Text.Trim();
-                happening.type = "text";
-                happening.idStory = (int)comboBoxStories.SelectedValue;
-
-                msg = HappeningOrm.Insert(happening);
-                if (msg != "")
+                happening happening = (happening)comboBoxHappenings.SelectedItem;
+                if (happening != null)
                 {
-                    MessageBox.Show(msg, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    string msg = HappeningOrm.Delete(happening);
+                    if (msg != "")
+                    {
+                        MessageBox.Show(msg, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MessageBox.Show("El evento se ha eliminado correctamente.", "Éxito!");
+                        Close();
+                    }
                 }
-                else
-                {
-                    MessageBox.Show("El evento se ha guardado correctamente.", "Éxito!");
-                    Close();
-                }
-                
-
-            }
-            else if (textBoxName.Text == "")
-            {
-                MessageBox.Show("No has introducido nombre de evento.", "Error");
-
-            } else if (comboBoxStories.SelectedItem == null)
-            {
-                MessageBox.Show("No has escogido la historia a la que pertenece.", "Error");
-
-            } else if(textBoxContent.Text == "")
-            {
-                MessageBox.Show("No has escrito nada en el contenido.", "Error");
-            }
-
+            }               
+            
         }
 
         private void buttonCancel_Click(object sender, EventArgs e)
@@ -111,13 +160,21 @@ namespace ARventure_Path.Forms
         {
             buttonCreate.Text = "Borrar";
             bindingSourceHappenings.DataSource = HappeningOrm.Select("text");
+            comboBoxHappenings.SelectedItem= null;
+            comboBoxStories.Enabled = false;
             Text = "Borrar evento de texto";
+            groupboxIA.Enabled = false;
+            groupboxTxtHappening.Enabled = false;
         }
 
         private void becomeInModifyForm()
         {
             buttonCreate.Text = "Guardar";
             bindingSourceHappenings.DataSource = HappeningOrm.Select("text");
+            comboBoxHappenings.SelectedItem = null;
+            comboBoxStories.Enabled = false;
+            groupboxIA.Enabled = false;
+            groupboxTxtHappening.Enabled = false;
             Text = "Modificar evento de texto";
         }
 
@@ -125,6 +182,23 @@ namespace ARventure_Path.Forms
         {
             labelSelectHappening.Visible = false;
             comboBoxHappenings.Visible = false;
+        }
+
+        private void comboBoxHappenings_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            happening happening = (happening)comboBoxHappenings.SelectedItem;
+            if(happening != null)
+            {
+                comboBoxStories.Enabled = true;
+                groupboxIA.Enabled = true;
+                groupboxTxtHappening.Enabled = true;
+                comboBoxStories.SelectedItem = happening.story;
+                textBoxName.Text = happening.name;
+                textBoxContent.Text = happening.text;
+            }
+           
+
         }
     }
 }
